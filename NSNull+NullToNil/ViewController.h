c0f7bdd09b93968a5578533e1d4c805b78fa3859
xlsn0w <xlsn0wios@gmail.com>
2018-06-22 11:19:00 +0800

    init

---
+++ NSNull+NullToNil/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSNull+NullToNil
+//
+//  Created by HL on 2018/6/22.
+//  Copyright © 2018年 XL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSNull+NullToNilTests/NSNull_NullToNilTests.m
@@ -0,0 +1,39 @@
+//
+//  NSNull_NullToNilTests.m
+//  NSNull+NullToNilTests
+//
+//  Created by HL on 2018/6/22.
+//  Copyright © 2018年 XL. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSNull_NullToNilTests : XCTestCase
+
+@end
+
+@implementation NSNull_NullToNilTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
